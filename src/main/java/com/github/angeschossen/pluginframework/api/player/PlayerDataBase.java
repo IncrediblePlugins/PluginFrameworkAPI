aefcc30643e1a45e46f14fc0e20f9d44eb540224
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2025-03-20 11:06:17 +0100

    update

--- src/main/java/com/github/angeschossen/pluginframework/api/player/PlayerDataBase.java
+++ src/main/java/com/github/angeschossen/pluginframework/api/player/PlayerDataBase.java
@@ -2,6 +2,7 @@ package com.github.angeschossen.pluginframework.api.player;
 
 import com.github.angeschossen.pluginframework.api.configuration.gui.GUIConfiguration;
 import com.github.angeschossen.pluginframework.api.configuration.messages.Messages;
+import com.github.angeschossen.pluginframework.api.server.ServerData;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Locale;
@@ -10,6 +11,8 @@ import java.util.UUID;
 public interface PlayerDataBase {
 
     @NotNull String getName();
+
+    @NotNull ServerData getServer();
 
     Object sendMessage(String msg, String[] p, String[] v);
 
