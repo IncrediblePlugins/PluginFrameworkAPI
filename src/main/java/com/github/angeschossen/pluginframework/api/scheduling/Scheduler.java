4a4b53a0dd82524db078ddf912696f449dc8fc0b
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2025-12-01 23:07:26 +0100

    update

--- src/main/java/com/github/angeschossen/pluginframework/api/scheduling/Scheduler.java
+++ src/main/java/com/github/angeschossen/pluginframework/api/scheduling/Scheduler.java
@@ -13,27 +13,27 @@ import java.util.concurrent.TimeUnit;
 
 public interface Scheduler {
 
-    @NotNull Task runTaskAsynchronously(@NotNull Runnable runnable);
+    @NotNull Task runTaskAsynchronously(@NotNull Runnable runnable,@NotNull String name);
 
-    @NotNull Task runTaskLaterAsynchronously(@NotNull Runnable runnable, long delay, @NotNull TimeUnit timeUnit);
+    @NotNull Task runTaskLaterAsynchronously(@NotNull Runnable runnable, long delay, @NotNull TimeUnit timeUnit,@NotNull String name);
 
-    @NotNull Task runTaskTimerAsynchronously(@NotNull Runnable runnable, long delay, long interval, @NotNull TimeUnit timeUnit);
+    @NotNull Task runTaskTimerAsynchronously(@NotNull Runnable runnable, long delay, long interval, @NotNull TimeUnit timeUnit,@NotNull String name);
 
-    @NotNull Task runTask(@NotNull Runnable runnable);
+    @NotNull Task runTask(@NotNull Runnable runnable,@NotNull String name);
 
-    @NotNull Task runTaskLater(@NotNull Runnable runnable, long delay, @NotNull TimeUnit timeUnit);
+    @NotNull Task runTaskLater(@NotNull Runnable runnable,long delay, @NotNull TimeUnit timeUnit,@NotNull String name);
 
-    @NotNull Task runTaskTimer(@NotNull Runnable runnable, long delay, long interval, @NotNull TimeUnit timeUnit);
+    @NotNull Task runTaskTimer(@NotNull Runnable runnable,long delay, long interval, @NotNull TimeUnit timeUnit,@NotNull String name);
 
-    @NotNull CompletableFuture<Void> runTaskAtLocation(@NotNull Location location, @NotNull Runnable runnable);
+    @NotNull CompletableFuture<Void> runTaskAtLocation(@NotNull Location location, @NotNull Runnable runnable,@NotNull String name);
 
-    @NotNull FutureTask<Void> runTaskAtLocationLater(@NotNull Location location, @NotNull Runnable runnable, long delay, @NotNull TimeUnit unit);
+    @NotNull FutureTask<Void> runTaskAtLocationLater(@NotNull Location location, @NotNull Runnable runnable, long delay, @NotNull TimeUnit unit,@NotNull String name);
 
-    @NotNull Task runTaskTimerAtLocation(@NotNull Location location, @NotNull Runnable runnable, long delay, long period, @NotNull TimeUnit unit);
+    @NotNull Task runTaskTimerAtLocation(@NotNull Location location, @NotNull Runnable runnable, long delay, long period, @NotNull TimeUnit unit,@NotNull String name);
 
-    @NotNull CompletableFuture<@NotNull EntityTaskResult> runTaskAtEntity(@NotNull Entity entity, @NotNull Runnable runnable, @Nullable Runnable retired);
+    @NotNull CompletableFuture<@NotNull EntityTaskResult> runTaskAtEntity(@NotNull Entity entity, @NotNull Runnable runnable, @Nullable Runnable retired,@NotNull String name);
 
-    @NotNull Task runTaskAtEntityLater(@NotNull Entity entity, @NotNull Runnable runnable, @Nullable Runnable retired, long delay, @NotNull TimeUnit unit);
+    @NotNull Task runTaskAtEntityLater(@NotNull Entity entity, @NotNull Runnable runnable, @Nullable Runnable retired, long delay, @NotNull TimeUnit unit,@NotNull String name);
 
-    @NotNull Task runTaskTimerAtEntity(@NotNull Entity entity, @NotNull Runnable runnable, @Nullable Runnable retired, long delay, long period, @NotNull TimeUnit unit);
+    @NotNull Task runTaskTimerAtEntity(@NotNull Entity entity, @NotNull Runnable runnable, @Nullable Runnable retired, long delay, long period, @NotNull TimeUnit unit,@NotNull String name);
 }
--- src/main/java/com/github/angeschossen/pluginframework/api/scheduling/task/Task.java
+++ src/main/java/com/github/angeschossen/pluginframework/api/scheduling/task/Task.java
@@ -1,10 +1,17 @@
 package com.github.angeschossen.pluginframework.api.scheduling.task;
 
+import org.jetbrains.annotations.NotNull;
+
 public interface Task {
 
     boolean isCancelled();
 
     boolean isSync();
 
+    @NotNull
+    String getName();
+
     void cancel();
+
+    boolean isPending();
 }
