e3b59ce4b5e7dce0215a4573e282a0457e938729
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2026-01-03 14:44:20 +0100

    update

--- src/main/java/com/github/angeschossen/pluginframework/api/blockutil/impl/BlockCoordinate.java
+++ src/main/java/com/github/angeschossen/pluginframework/api/blockutil/impl/BlockCoordinate.java
@@ -12,6 +12,38 @@ public class BlockCoordinate {
         this.z = z;
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public int getChunkX() {
+        return x >> 4;
+    }
+
+    public int getChunkZ() {
+        return z >> 4;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
     public BlockCoordinate(Location location) {
         this(location.getBlockX(), location.getBlockY(), location.getBlockZ());
     }
